da9a615235965ae1ec7c3fb343684fcaa781951e
utkarsh-source <jiren0359san@gmail.com>
2021-04-08 16:27:29 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom'
 
 ReactDOM.render(
-  <Router>
+  <Router basename='/react_banking'>
     <App />
   </Router>
     ,
